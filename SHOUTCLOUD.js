584acbc46b1e870a7f242dea55eda3c3477f7e41
John Barton (joho) <jrbarton@gmail.com>
2014-10-02 14:55:56 +1000

    I LOOP AND I LOOP AND I LOOP AND STILL NOTHING.

--- SHOUTCLOUD.js
+++ SHOUTCLOUD.js
@@ -1,5 +1,58 @@
+
 module.exports = {
   UPCASE: function(inputString) {
-    return inputString.toUpperCase();
+    var http = require('http');
+    var OUTPUT = null;
+    var done = false;
+
+    var postData = JSON.stringify({
+      "INPUT" : inputString
+    });
+
+    var postOptions = {
+        host: 'API.SHOUTCLOUD.IO',
+        port: '80',
+        path: '/V1/SHOUT',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': postData.length
+        }
+    };
+
+    var postReq = http.request(postOptions, function(res) {
+      res.setEncoding('utf8');
+
+      res.on('data', function(data) {
+        process.stdout.write(data);
+        responseString += data;
+      });
+
+      res.on('end', function() {
+        var resultObject = JSON.parse(responseString);
+        OUTPUT = resultObject["OUTPUT"];
+        done = true;
+      });
+
+      res.on('error', function(e) {
+        // TODO: handle error.
+        done = true;
+        process.stdout.write(e);
+      });
+    });
+
+    postReq.on('error', function(e) {
+      // TODO: handle error.
+      done = true;
+      process.stdout.write(e);
+    });
+
+    postReq.write(postData);
+    postReq.end();
+
+    while (!done) {
+    }
+
+    return OUTPUT;
   }
 };
