16dce8d02cb22479f9a7ccc98656b997c4ee5589
John Barton (joho) <jrbarton@gmail.com>
2014-10-03 08:09:52 +1000

    WHEN IN DOUBT, WRAP LIBCURL

--- SHOUTCLOUD.js
+++ SHOUTCLOUD.js
@@ -1,57 +1,29 @@
 
 module.exports = {
   UPCASE: function(inputString) {
-    var http = require('http');
+    var http = require('httpsync');
     var OUTPUT = null;
-    var done = false;
 
     var postData = JSON.stringify({
       "INPUT" : inputString
     });
 
     var postOptions = {
-        host: 'API.SHOUTCLOUD.IO',
-        port: '80',
-        path: '/V1/SHOUT',
+        url: "http://API.SHOUTCLOUD.IO/V1/SHOUT",
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Content-Length': postData.length
-        }
+        },
+        timeout: 5
     };
 
-    var postReq = http.request(postOptions, function(res) {
-      res.setEncoding('utf8');
-
-      res.on('data', function(data) {
-        process.stdout.write(data);
-        responseString += data;
-      });
-
-      res.on('end', function() {
-        var resultObject = JSON.parse(responseString);
-        OUTPUT = resultObject["OUTPUT"];
-        done = true;
-      });
-
-      res.on('error', function(e) {
-        // TODO: handle error.
-        done = true;
-        process.stdout.write(e);
-      });
-    });
-
-    postReq.on('error', function(e) {
-      // TODO: handle error.
-      done = true;
-      process.stdout.write(e);
-    });
-
+    var postReq = http.request(postOptions);
     postReq.write(postData);
-    postReq.end();
+    var response = postReq.end();
 
-    while (!done) {
-    }
+    var resultObject = JSON.parse(response.data);
+    OUTPUT = resultObject["OUTPUT"];
 
     return OUTPUT;
   }
